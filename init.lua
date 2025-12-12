d2f2c6c8a6f15cce9210161701861b241d6be4e3
varunpenumudi <penumudivarun@gmail.com>
2025-12-12 08:22:37 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("config.lazy")
+
+-- line numbers 
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- tabs and spaces
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- blocky cursor 
+vim.o.guicursor = "n-v-c:block,i:block-blinkwait100-blinkoff400-blinkon250"
+
