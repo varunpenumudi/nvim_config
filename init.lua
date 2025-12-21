5e6440e2bb6619326fe89d3b883c32a885061598
varunpenumudi <penumudivarun@gmail.com>
2025-12-21 11:52:07 +0530

    Added keymaps :)

--- init.lua
+++ init.lua
@@ -12,9 +12,14 @@ vim.o.expandtab = true
 -- blocky cursor 
 vim.o.guicursor = "n-v-c:block,i:block-blinkwait100-blinkoff400-blinkon250"
 
-
 -- windows
 vim.o.splitbelow = true
 vim.o.splitright = true
 vim.keymap.set("n", "<leader>n", ":vnew<CR>")
 vim.keymap.set("n", "<leader>N", ":new<CR>")
+
+-- quality of life keymaps
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
