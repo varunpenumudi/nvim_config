bea10633267291535b202f31cf81989c7399d827
varunpenumudi <penumudivarun@gmail.com>
2026-01-11 19:27:14 +0530

    Updated lsp config to be more stable

--- init.lua
+++ init.lua
@@ -23,4 +23,3 @@ vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
-
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,12 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = {"saghen/blink.nvim"},
+    dependencies = {
+        "saghen/blink.nvim",                 -- autocomplete
+        {"folke/lazydev.nvim", opts={}},     -- detect vim as global
+    },
     config = function()
-        -- lua config
-        vim.lsp.config("lua_ls", {
-              cmd = { 'lua-language-server' },
-              filetypes = { 'lua' },
-              root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-              capabilities = require('blink.cmp').get_lsp_capabilities(),
-              settings = {
-                  Lua = {
-                      runtime = {
-                          version = 'LuaJIT',
-                      },
-                      -- 1. Tell the server that 'vim' is a global variable
-                      diagnostics = {
-                          globals = { 'vim' },
-                      },
-                      -- 2. (Optional but recommended) Add Neovim runtime files to workspace 
-                      workspace = {
-                          library = vim.api.nvim_get_runtime_file("", true),
-                          checkThirdParty = false, -- Stops the "Do you want to configure environment?" prompt
-                      },
-                      telemetry = {
-                          enable = false,
-                      },
-                  }
-              }
-        })
-
-        vim.lsp.enable("clangd", {
+        -- autocomplete
+        vim.lsp.config("*", {
              capabilities = require('blink.cmp').get_lsp_capabilities(),
         })
 
@@ -37,7 +14,7 @@ return {
         vim.lsp.enable("lua_ls")
         vim.lsp.enable("clangd")
 
-        -- autocomplete
+        -- diagnostic 
         vim.diagnostic.config({ virtual_text = true })
     end
 }
